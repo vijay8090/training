890a932db8737613ecce9083f49208cbf06ee189
vijay8090 <vijayakumar.anbu@yahoo.com>
2016-01-17 23:55:04 +0530

    hibernate sample initial files

---
+++ projects/hibernatesample/dbscript/sqlscript.sql
@@ -0,0 +1,6 @@
+create table EMPLOYEE (    
+id INT NOT NULL auto_increment,    
+first_name VARCHAR(20) default NULL,   
+last_name  VARCHAR(20) default NULL,    
+salary     INT  default NULL,   
+PRIMARY KEY (id) ); 
