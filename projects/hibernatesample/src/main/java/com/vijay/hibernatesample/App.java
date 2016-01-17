429e39983a57a02a73ddf8f5312d6ab884b2ef32
vijay8090 <vijayakumar.anbu@yahoo.com>
2016-01-17 23:51:21 +0530

    hibernate sample initial files

---
+++ projects/hibernatesample/src/main/java/com/vijay/hibernatesample/App.java
@@ -0,0 +1,13 @@
+package com.vijay.hibernatesample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
